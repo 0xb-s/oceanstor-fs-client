ab1535a3a8342b681fbee425cce14d93a834e716
0xb-s <145866191+0xb-s@users.noreply.github.com>
2025-06-03 13:30:26 -0700

    Document source of API implementation (#1)

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,6 @@ mod util;
 
 pub use client::OceanStor;
 
-// Source : https://support.huawei.com/enterprise/en/doc/EDOC1100156021/3e592917/file_system#EN-US_CLIREF_0000001125240121
+
+// The entire implementation is based on the official Huawei OceanStor documentation.
+// Source: https://support.huawei.com/enterprise/en/doc/EDOC1100156021/3e592917/file_system#EN-US_CLIREF_0000001125240121
